769709b8185d3a7eac44d612495b3b7fe461c5c5
mhend56 <mhend56@gmail.com>
2023-07-12 13:13:31 +1200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+import Form from './Form';
+import ColorList from './ColorList';
+import Values from 'values.js';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
+const App = () => {
+  const [colors, setColors] = useState(new Values('#f15025').all(10));
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(20);
+      setColors(newColors);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  return (
+    <main>
+      <Form addColor={addColor} />
+      <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
+    </main>
+  );
+};
+export default App;
