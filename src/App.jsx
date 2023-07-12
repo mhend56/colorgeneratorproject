1ee77b2723bd75494938f129e30690439bed648b
mhend56 <mhend56@gmail.com>
2023-07-12 13:21:26 +1200

    Change1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
-  const [colors, setColors] = useState(new Values('#f15025').all(10));
+  const [colors, setColors] = useState(new Values('#f4e0b9').all(10));
   const addColor = (color) => {
     try {
       const newColors = new Values(color).all(20);
